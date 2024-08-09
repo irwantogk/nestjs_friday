683178bcfe2123c8c5113771ddb06c1654c0834b
irwantogk <irwantowibowo1992@gmail.com>
2024-08-10 00:59:47 +0700

    feat: add interceptor for response

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { log } from 'console';
+import { CustomResponseInterceptor } from './commen/interceptor/tramsform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalInterceptors(new CustomResponseInterceptor());
+  await app.listen(3000, () => log(`Server running on port 3000`));
 }
 bootstrap();
