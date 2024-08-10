e7a0a88029620da1f84b0a93a6ae2100fe7e886f
irwantogk <irwantowibowo1992@gmail.com>
2024-08-10 22:55:00 +0700

    config: config documentation API with swagger

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { log } from 'console';
 import { CustomResponseInterceptor } from './common/interceptor/tramsform.interceptor';
+import { appConfig } from './config/app.config';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new CustomResponseInterceptor());
-  await app.listen(3000, () => log(`Server running on port 3000`));
+
+  if (appConfig.environment === 'development') {
+    const document = SwaggerModule.createDocument(app, appConfig.documentation);
+    SwaggerModule.setup('openapi', app, document);
+  }
+
+  const port = appConfig.port;
+  await app.listen(port, () => log(`Server running on port ${port}`));
 }
 bootstrap();
