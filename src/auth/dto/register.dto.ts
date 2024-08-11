92e05f87a081b5062f5e597814d684f3da8881db
irwantogk <irwantowibowo1992@gmail.com>
2024-08-11 13:41:58 +0700

    feat: add validation

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,11 +1,34 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class RegisterDto {
+  @IsNotEmpty()
+  @IsString()
   firstName: string;
 
+  @IsOptional()
+  @IsString()
   lastName: string;
 
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(30)
   username: string;
 
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  @MaxLength(30)
   password: string;
 }
